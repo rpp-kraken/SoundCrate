d00fd343d222f2a77fbfb858d2438c47f878d4ed
mindiweik <mindiweik@gmail.com>
2023-03-09 22:40:19 -0700

    Added Google Oauth to npm dependencies, began working on the Google Client ID secret

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Login from '../components/login/Login.jsx'
 
-export default function() {
+export default function(props) {
   return (
     <div>
       Hello World!
+      {props.clientID}
       <Login />
     </div>
   );
