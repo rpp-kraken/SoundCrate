053fb84d33633f4f2d3abb1acba59b78c302008e
mindiweik <mindiweik@gmail.com>
2023-03-09 21:45:33 -0700

    Initial connection

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Login from '../components/login/Login.jsx'
 
 export default function() {
   return (
     <div>
       Hello World!
+      <Login />
     </div>
   );
 }
