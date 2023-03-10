c9931f832e89965262b7d63fca88d673054b142e
mindiweik <mindiweik@gmail.com>
2023-03-10 13:10:00 -0700

    Google sign in option appears, but isn't connecting properly

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+
 
 export default function Login(props) {
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
+
   return (
     <div>
-      Login feature
+      <h2>Google Login</h2>
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
     </div>
   );
 }
