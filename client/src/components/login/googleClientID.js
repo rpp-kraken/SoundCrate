62f7f9fad500fd397ce4529b691fddbe585b1bfe
mindiweik <mindiweik@gmail.com>
2023-03-09 22:40:19 -0700

    Added Google Oauth to npm dependencies, began working on the Google Client ID secret

---
+++ client/src/components/login/googleClientID.js
@@ -0,0 +1,1 @@
+module.exports.clientID = process.env.GOOGLE_CLIENTID
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.render(<App />, document.getElementById('root'));
