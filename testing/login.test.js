70aba7664ae79c6f3789e34c5a8c6f80264beee2
mindiweik <mindiweik@gmail.com>
2023-03-12 16:31:24 -0600

    connected React Testling Library and setup config needed

--- testing/login.test.js
+++ testing/login.test.js
@@ -1,5 +1,15 @@
-describe('example test', () => {
-  it('should assert true is true', () => {
+/**
+ * @jest-environment jsdom
+ */
+
+import { fireEvent, render, screen } from '@testing-library/react'
+import { queries } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import React, { useEffect, useState } from "react"
+
+describe('Login test', () => {
+  it('Google Oauth button should be present in the drawer', () => {
     expect(true).toBe(true);
   });
 });
