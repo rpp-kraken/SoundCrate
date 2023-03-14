589f08d505f6e79d30af50c041c7181b3a0cd622
Sean McDaniel <sm.fm.static@gmail.com>
2023-03-13 22:55:43 -0400

    Added appropriate database information in db file as well as env file. Installed dependencies for testing and set up songs server testing file

---
+++ testing/songsRoutes.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment node
+ */
+
+const { expect } = require('chai');
+const request = require('supertest');
+const Pool = require('pg-pool');
+const client = require('../db/index');
+const app = require('../server/index');
+
+describe('Songs upload route', () => {
+
+  //Mocking db connection and loading app
+  before(async () => {
+    const pool = new Pool({
+      host: '127.0.0.1',
+    port: 5432,
+    database: 'soundcrate',
+    user: 'postgres',
+    password: '',
+    max: 1,
+    idleTimeoutMillis: 0
+    });
+
+    client.query = (text, values) => {
+      return pool.query(text, values);
+    }
+  });
+});
