f812318acfa3aac5fbace8316ab7e350321f9cc1
Sean McDaniel <sm.fm.static@gmail.com>
2023-03-13 22:33:57 -0400

    Added credentials for s3 bucket in env file, install dotenv to project, set up initial folders within server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./client/dist'));
+const upload = multer();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//ROUTES
+app.post('/upload', upload.fields([
+  {name: 'audioFile', maxCount: 1},
+  {name: 'imageFile', maxCount: 1}
+]), handleUpload);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
