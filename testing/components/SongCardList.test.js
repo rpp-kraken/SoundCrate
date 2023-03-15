a6ebf36d88534f30aa10803ba22d87cc17fdd44f
Nate Foster <nate.s.foster@gmail.com>
2023-03-15 10:28:28 -1000

    add coverage in testing

---
+++ testing/components/SongCardList.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+describe('SongCardList', () => {
+  const renderSongCardList = (songs) => render(<SongCardList songs={songs} />);
+
+  it('should render a SongCardList to the screen', () => {
+
+  });
+});
