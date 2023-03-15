15c661630236d22fad93b900e40a5e0a72c64043
Calvin Goodale <calvin.goodale@gmail.com>
2023-03-14 22:46:09 -0400

    Added more component skeletons and conditional rendering state

---
+++ client/src/components/Discover.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { createTheme } from '@mui/material/styles';
+
+
+export default function Discover() {
+  return (
+    <div>
+      <h3>Discover view</h3>
+    </div>
+  );
+}
