f990847568f40dcc656ced3d4cf2ace80216c827
Nate Foster <nate.s.foster@gmail.com>
2023-03-16 17:23:44 -1000

    implement search bar functionality on discover tab

--- client/src/components/Discover.jsx
+++ client/src/components/Discover.jsx
@@ -4,13 +4,22 @@ import SongCardList from './SongCardList.jsx';
 import Search from './Search.jsx';
 import { songData } from '../../../DummyData/dummyData.js';
 
+const filterData = (query, data) => {
+  return !query ? data : data.filter((d) => {
+    return d.title.toLowerCase().includes(query.toLowerCase()) ||
+      d.user_id.toLowerCase().includes(query.toLowerCase())
+  });
+}
+
 export default function Discover() {
+  const [searchQuery, setSearchQuery] = useState("");
   // query the database for the first ten songs
   // for now, just use dummy data
+  let songs = filterData(searchQuery, songData);
   return (
     <div className="discover">
-      <Search />
-      <SongCardList songs={songData} />
+      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <SongCardList songs={songs} />
     </div>
   )
 }
