ba9405d2f3ff934c1376be83b5ef1a7c8d31ef4b
Sean McDaniel <sm.fm.static@gmail.com>
2023-03-17 12:03:46 -0400

    Refactored addSong to handle testing for temporary tables. I'll be passing in the temporary tables as arguments when testing, otherwise the tables will default to the correct table names. Also, if we're in testing mode, the db will change to the testing db connection, rather than the standard connection

--- server/models/index.js
+++ server/models/index.js
@@ -2,10 +2,11 @@ const db = require('../../db/index');
 const { uuid } = require('uuidv4');
 
 module.exports = {
-  addSong: async (data) => {
+  addSong: async (data, songsTable = 'songs', usersTable = 'users') => {
+    db = process.env.NODE_ENV === 'test' ? global.client : db;
     const songId = uuid();
-    var user_id =  await db.query(`SELECT id FROM users WHERE name = '${data.user}'`);
-    return db.query(`INSERT INTO songs (id, title, created_at, path_to_song, play_count, fav_count, path_to_artwork, user_id)
+    var user_id =  await db.query(`SELECT id FROM ${usersTable} WHERE name = '${data.user}'`);
+    return db.query(`INSERT INTO ${songsTable} (id, title, created_at, path_to_song, play_count, fav_count, path_to_artwork, user_id)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`, [songId, data.title, data.created_at, data.path_to_song, data.play_count,
       data.fav_count, data.path_to_artwork, user_id.rows[0].id]);
   },
