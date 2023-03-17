179037408087776feb4256ca5b1db9f99b1e84e3
Nate Foster <nate.s.foster@gmail.com>
2023-03-16 17:11:08 -1000

    Style Search Bar with material ui

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 
-export default function Search() {
+export default function Search({setSearchQuery}) {
   return (
-    <div className="searchbar">
-      <input type="text" placeholder="Search..."></input>
-    </div>
+    <form className="searchbar">
+      <TextField
+        id="search-bar"
+        className="search-bar-text"
+        onInput={(e) => setSearchQuery(e.target.value)}
+        variant="outlined"
+        placeholder="Search..."
+        size="small"
+      />
+      <IconButton type="submit" aria-label="search">
+        <SearchIcon style={{ fill: "blue" }} />
+      </IconButton>
+    </form>
+    // <input type="text" placeholder="Search..."></input>
   );
 }
