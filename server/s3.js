dd9d041ecfa4a425475f8e4817456b900e45f64f
Sean McDaniel <sm.fm.static@gmail.com>
2023-03-17 15:00:10 -0400

    Refactored to s3 file to pull bucket credentials from env file and explicitly state bucket name

--- server/s3.js
+++ server/s3.js
@@ -5,15 +5,15 @@ const config = require('../config');
 
 // Connection to S3 bucket
 const s3 = new AWS.S3({
-    accessKeyId: config.accessKeyId,
-    secretAccessKey: config.secretAccessKey,
-    region: config.region
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: process.env.REGION
 });
 
 //Handles iPhone/Android recording files -- url returned is now publicly available
 const uploadAudioFile = (audioFileData) => {
   const params = {
-      Bucket: config.bucketName,
+      Bucket: 'soundcrate',
       Key: `${uuid()}.m4a`,
       Body: audioFileData,
       ContentType: 'audio/mp4',
@@ -33,7 +33,7 @@ const uploadAudioFile = (audioFileData) => {
 //Handles jpeg images
 const uploadImageFile = (imageFileData) => {
     const params = {
-        Bucket: config.bucketName,
+        Bucket: 'soundcrate',
         Key: `${uuid()}.jpeg`,
         Body: imageFileData,
         ContentType: 'image/jpeg',
