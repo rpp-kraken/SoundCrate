163166c14dafd3c8ca4ab573d68c5ae37d40773c
Brian Kuzma <78782770+kuzmabr@users.noreply.github.com>
2023-03-19 20:21:32 -0700

    commented out HTTPS

--- server/index.js
+++ server/index.js
@@ -1,26 +1,47 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const multer = require('multer');
 const { handleUpload } = require('./controllers/handleUpload');
 const { getSongs } = require('./controllers/getSongs');
 const upload = multer();
 
+const app = express();
+
 app.use(express.static('./client/dist'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//ROUTES
+// ROUTES
 app.get('/api/songs', getSongs);
-app.post('/api/uploadSong', upload.fields([
-  {name: 'audioFile', maxCount: 1},
-  {name: 'imageFile', maxCount: 1}
-]), handleUpload);
+app.post(
+  '/api/uploadSong',
+  upload.fields([
+    { name: 'audioFile', maxCount: 1 },
+    { name: 'imageFile', maxCount: 1 },
+  ]),
+  handleUpload
+);
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}...`);
+// Need SSL KEY AND CERTS
+const httpPort = 3000;
+const httpsPort = 443;
+// const key = fs.readFileSync(process.env.SSL_KEY_PATH);
+// const cert = fs.readFileSync(process.env.SSL_CERT_PATH);
+
+
+const httpServer = http.createServer(app);
+// const httpsServer = https.createServer({ key, cert }, app);
+
+httpServer.listen(httpPort, () => {
+  console.log(`HTTP server listening on port ${httpPort}...`);
 });
+
+// httpsServer.listen(httpsPort, () => {
+//   console.log(`HTTPS server listening on port ${httpsPort}...`);
+// });
 
 module.exports = app;
