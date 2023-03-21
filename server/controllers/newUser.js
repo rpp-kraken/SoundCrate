47fe2b8a8440e11f6a52abdb07aafbe93e61a9a9
mindiweik <mindiweik@gmail.com>
2023-03-20 21:51:06 -0600

    backtrack on sync

---
+++ server/controllers/newUser.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const { uploadAudioFile, uploadImageFile, s3 } = require('../s3');
+const models = require('../models/index');
+
+const newUser = async (req, res) => {
+  const data = req.body;
+  const usersTable = process.env.NODE_ENV === 'test' ? 'temp_users' : 'users';
+
+  try {
+    if (req.body.imageFile !== '') {
+      const imageFileData = req.files['imageFile'][0].buffer || '';
+      await uploadImageFile(imageFileData)
+        .then(imageFileUrl => {
+          console.log('image file url', imageFileUrl)
+          data.path_to_pic = imageFileUrl;
+        });
+    }
+
+    await models.addUser(data, usersTable);
+    res.status(201).json('successfully added new user');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to add new user' });
+  }
+};
+
+module.exports = {
+  newUser
+}
