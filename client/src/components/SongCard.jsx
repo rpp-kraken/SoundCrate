82bbc2d23fded994675aa2557f34df6a39987a1a
Aaron Miller <AaronMiller1855@gmail.com>
2023-03-21 18:47:14 -0700

    add png images and start work on conditioanl icon r endering

--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -7,6 +7,7 @@ import Box from '@mui/material/Box';
 import Play from './Play.jsx';
 import Stack from '@mui/material/Stack';
 
+
 const useStyles = makeStyles({
   card: {
     display: 'flex',
@@ -40,7 +41,17 @@ export default function SongCard({ title, artist, path_to_song, artistImageUrl, 
 
   const classes = useStyles();
 
-
+  // const returnIcon = (count) => {
+  //   if(count >= 250000 && count <= 499000) {
+  //     return 'bronze'
+  //   }
+  //   if(count >= 500000 && count <= 999999) {
+  //     return <img src={'/client/dist/silver-icon-badge.PNG'} />
+  //   }
+  //   if(count >= 1000000) {
+  //     return 'gold'
+  //   }
+  // }
 
   // Favorite Song Event Handling
   const handleLikeClick = (event) => {
@@ -51,91 +62,64 @@ export default function SongCard({ title, artist, path_to_song, artistImageUrl, 
 
   return (
     <Card
-  className={classes.card}
-  raised
-  style={{
-    margin: '15px',
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.text.primary,
-    borderRadius: 0,
-    minHeight: '16vh',
-    maxHeight: '16vh',
-    cursor: 'pointer',
-    transition: 'transform 0.2s ease-in-out',
-    '&:hover': {
-      transform: 'scale(1.02)',
-    },
-  }}
-  onClick={() => {
-        handleSetArtistSongData(null,
-          {
-            title,
-            artist,
-            artistImageUrl,
-            likedCount,
-            play_count,
-            path_to_song
-          }
-        );
+      className={classes.card}
+      raised
+      style={{
+        margin: '15px',
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.text.primary,
+        borderRadius: 0,
+        minHeight: '16vh',
+        maxHeight: '16vh',
+        cursor: 'pointer',
+        transition: 'transform 0.2s ease-in-out',
+        '&:hover': {
+          transform: 'scale(1.02)',
+        },
+      }}
+      onClick={() => {
+        handleSetArtistSongData(null, {
+          title,
+          artist,
+          artistImageUrl,
+          likedCount,
+          play_count,
+          path_to_song
+        });
         changeView('play');
-      }
-      }
->
-  {/* {playViewOpen && (
-    <Play
-      title={title}
-      artist={artist}
-      artistImageUrl={artistImageUrl}
-      likedCount={likedCount}
-      playCount={playCount}
-      trackUrl={path_to_song}
-      handleClose={handleClosePlayView}
-    />
-  )}
-
-      {otherArtistViewOpen && <ProfileOtherArtist
-        artist={artist}
-        handleClose={handleOtherArtistProfileClose} />} */}
-
-  <CardContent className={classes.content}>
-    <Typography variant="h5" component="h5" style={{ fontSize: '1.25rem' }}>
-      {title}
-    </Typography>
-    <Typography
-      variant="subtitle1"
-      onClick={(event) => {
-            event.stopPropagation();
-            handleSetArtistSongData(
-              {
-                artist
-              }, null
-            );
-            changeView('profile');
-          }
-          }
-      style={{ cursor: 'pointer', marginTop: '8px' }}
+      }}
     >
-      {artist}
-    </Typography>
-    <Stack direction="row" alignItems="center" spacing={2} mt={1}>
-      <IconButton onClick={handleLikeClick}>
-        {liked ? <Favorite /> : <FavoriteBorder />}
-      </IconButton>
-      <Typography variant="body2" color="textSecondary" component="span">
-        {likedCount}
-      </Typography>
-      <Typography variant="body2" color="textSecondary" component="span">
-      🎵 {play_count} Plays
-      </Typography>
-    </Stack>
-  </CardContent>
-  <CardMedia className={classes.media} component="img" image={`${artistImageUrl}`} />
-</Card>
-
-
-
-
-
-
+      <CardContent className={classes.content}>
+        <Typography variant="h5" component="h5" style={{ fontSize: '1.25rem' }}>
+          {title}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          onClick={(event) => {
+            event.stopPropagation();
+            handleSetArtistSongData({ artist }, null);
+            changeView('profile');
+          }}
+          style={{ cursor: 'pointer', marginTop: '8px' }}
+        >
+          {artist}
+        </Typography>
+        <Stack direction="row" alignItems="center" spacing={1} mt={1}>
+          <IconButton onClick={handleLikeClick}>
+            {liked ? <Favorite /> : <FavoriteBorder />}
+          </IconButton>
+          <Typography variant="body2" color="textSecondary" component="span">
+            {likedCount}
+            {likedCount >= 250000 && likedCount <= 499999 && <img src={'silver.png'} className="badge-icon"/>}
+            {likedCount >= 500000 && likedCount <= 999999 && <img src={'bronze-icon-badge.png'} className="badge-icon"/>}
+            {likedCount >= 1000000 && <img src={'gold-icon-badge.png'} className="badge-icon"/>}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="span">
+            {`🎧 ${play_count}`}
+          </Typography>
+        </Stack>
+      </CardContent>
+      <CardMedia className={classes.media} component="img" image={`${artistImageUrl}`} />
+    </Card>
   );
 }
