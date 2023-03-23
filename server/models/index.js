27b342c4bccc9ead28f2a921e1184893bf9b775a
Sean McDaniel <sm.fm.static@gmail.com>
2023-03-23 16:04:30 -0400

    Refactored db query to grab tags for each song and return empty array if no tags exist

--- server/models/index.js
+++ server/models/index.js
@@ -87,8 +87,25 @@ const deleteSong = async (songId) => {
 
 const getUsersFavoriteSongs = async (user) => {
   const userId = await db.query(`SELECT id FROM ${usersTable} WHERE name = $1`, [user]);
-  return db.query(`SELECT songs.*, users.id AS user_id FROM ${usersTable} JOIN favorites ON users.id = favorites.user_id
-    JOIN songs ON favorites.song_id = songs.id WHERE users.id = $1`, [userId.rows[0].id]);
+  // return db.query(`SELECT songs.*, users.id AS user_id FROM ${usersTable} JOIN favorites ON users.id = favorites.user_id
+  //   JOIN songs ON favorites.song_id = songs.id WHERE users.id = $1`, [userId.rows[0].id]);
+  return db.query(`SELECT
+    songs.*,
+    users.id AS user_id,
+    COALESCE(ARRAY_AGG(song_tags.name) FILTER (WHERE song_tags.name IS NOT NULL), ARRAY[]::text[]) AS tags
+  FROM
+    users
+  JOIN
+    favorites ON users.id = favorites.user_id
+  JOIN
+    songs ON favorites.song_id = songs.id
+  LEFT JOIN
+    song_tags ON songs.id = song_tags.song_id
+  WHERE
+    users.id = $1
+  GROUP BY
+    songs.id, users.id;
+`, [userId.rows[0].id]);
 };
 
 module.exports = {
