44113ae05a00866f2de44e6fcde757c8a65e67a6
Aaron Miller <AaronMiller1855@gmail.com>
2023-03-24 11:47:56 -0700

    adjust splash page to be optimized to mobile view and add modal to login after video ends

--- client/src/components/login/Splash.jsx
+++ client/src/components/login/Splash.jsx
@@ -1,29 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import '../../../dist/Video.css';
+import Login from './Login.jsx'
 
 
 export default function Splash() {
-  const youTubeStyle = {
-    overflow: 'hidden',
-    paddingBottom: '56.25%',
-    position: 'relative',
-    height: '0'
-  }
 
-  const iframeStyle = {
-    left: '0',
-    top: '0',
-    height: '100%',
-    width: '100%',
-    position: 'absolute'
-  }
+  const [showModal, setModal] = useState(false)
 
   return (
-    <div style={youTubeStyle}>
-      {/* Google Drive */}
-      <iframe style={iframeStyle} src="https://drive.google.com/file/d/1oHRFh7fYUlh9b_koh4WHqzYTkmm0pPf2/preview" width="640" height="480" allow="autoplay"></iframe>
-
-      {/* YouTube */}
-      {/* <iframe style={iframeStyle} width="560" height="315" src="https://www.youtube.com/embed/6hNn3_r_MT4?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+    <div>
+      <video autoPlay muted className="video" onEnded={() => setModal(true)}>
+        <source src="SquareNeonOpener.mp4" type="video/mp4" />
+        Ouch! Sorry, your browser doesnt support this video! Get logged in to have some fun!
+      </video>
+      {showModal && <Login />}
     </div>
   );
 }
