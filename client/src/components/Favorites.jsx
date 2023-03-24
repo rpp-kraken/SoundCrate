73c7b54a9f4cfccbb31211ddff4438dd8645050e
Sean McDaniel <sm.fm.static@gmail.com>
2023-03-24 11:00:16 -0400

    Set up useEffect in Favorites component and passed down relevant profileData to it from App

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -2,7 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { createTheme } from '@mui/material/styles';
 
 export default function Favorites(props) {
-
+  console.log('these are the fav props: ', props);
+  const [songs, setSongs] = useState();
+  useEffect(() => {
+    fetch(`http://localhost:3000/getFavoriteSongs?user=${encodeURIComponent(props.profileData.name)}`)
+      .then(songs => {
+        console.log(songs);
+        setSongs(songs);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [props.profileData.name]);
   return (
     <div>
       <h3>Favorites view - list of user's favorited songs</h3>
