da23b8396583a181579e9b6c9aa3e26239797394
mindiweik <mindiweik@gmail.com>
2023-03-23 20:35:41 -0600

    add buttons to logout and initial state change

--- client/src/components/ConfirmLogOut.jsx
+++ client/src/components/ConfirmLogOut.jsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { createTheme } from '@mui/material/styles';
+import { Button } from '@mui/material';
 
-export default function ConfirmLogOut(props) {
+export default function ConfirmLogOut({ setLoggedIn }) {
+  const handleLogout = () => {
+    console.log('log out');
+    setLoggedIn(false);
+  }
+
+  const handleCancel = () => {
+    console.log('no thanks')
+  }
 
   return (
     <div>
-      <h3>ConfirmLogOut view - Are you sure you want to log out?</h3>
+      <h3>Are you sure you want to log out?</h3>
+      <Button variant="contained" component="label">
+        Yes
+        <input type="submit" onClick={handleLogout} style={{ display: 'none' }}/>
+      </Button>
+      <Button variant="contained" component="label">
+        No
+        <input type="submit" onClick={handleCancel} style={{ display: 'none' }}/>
+      </Button>
     </div>
   );
 }
