8226f2a3f153aa5601e97615088e79fd6e12061b
Aaron Miller <AaronMiller1855@gmail.com>
2023-03-27 17:01:41 -0700

    add snarky splash message for favorites page

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -5,6 +5,12 @@ import SongCardList from './SongCardList.jsx';
 export default function Favorites(props) {
   const [songs, setSongs] = useState();
   const [loading, setLoading] = useState(true);
+  <div>
+  <video autoPlay muted className="video" >
+    <source src="snarkyFavoritesSplash.mp4" type="video/mp4" />
+    Ouch! Sorry, your browser doesnt support this video! Get logged in to have some fun!
+  </video>
+</div>
   useEffect(() => {
     fetch(`http://localhost:3000/api/getFavoriteSongs?user=${encodeURIComponent(props.profileData.name)}`)
       .then(res => {
@@ -23,9 +29,19 @@ export default function Favorites(props) {
   }, [props.profileData.name]);
   return (
     <div>
-      {loading
-        ? <p>Loading...</p>
-        : <SongCardList songs={songs} />}
+              <video autoPlay muted className="video" >
+          <source src="snarkyFavoritesSplash.mp4" type="video/mp4" />
+          Ouch! Sorry, your browser doesnt support this video! Get logged in to have some fun!
+        </video>
+      {/* {loading
+        ?
+        <div>
+        <video autoPlay muted className="video" >
+          <source src="snarkyFavoritesSplash.mp4" type="video/mp4" />
+          Ouch! Sorry, your browser doesnt support this video! Get logged in to have some fun!
+        </video>
+      </div>
+        : <SongCardList songs={songs} />} */}
     </div>
   );
 };
