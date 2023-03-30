37c07b51f46306712231a1229b8622fc32af1893
Aaron Miller <AaronMiller1855@gmail.com>
2023-03-30 15:56:57 -0700

    update favorites tab logic to show snarky splash video -- logic adjustments

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -3,14 +3,8 @@ import { createTheme } from '@mui/material/styles';
 import SongCardList from './SongCardList.jsx';
 
 export default function Favorites(props) {
-  const [songs, setSongs] = useState();
+  const [songs, setSongs] = useState([]);
   const [loading, setLoading] = useState(true);
-  <div>
-  <video autoPlay muted className="video" >
-    <source src="snarkyFavoritesSplash.mp4" type="video/mp4" />
-    Ouch! Sorry, your browser doesnt support this video! Get logged in to have some fun!
-  </video>
-</div>
   useEffect(() => {
     fetch(`http://localhost:3000/api/getFavoriteSongs?user=${encodeURIComponent(props.profileData.name)}`)
       .then(res => {
@@ -29,7 +23,7 @@ export default function Favorites(props) {
   }, [props.profileData.name]);
   return (
     <div className="discover" >
-      {loading
+      {songs.length === 0
         ?
         <div>
         <video autoPlay muted className="video" >
